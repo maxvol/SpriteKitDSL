e18a2812989068142f97d7c6f0aed8a2c36b0d41
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 20:01:18 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+  
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SpriteKitDSL",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "SpriteKitDSL",
+            targets: ["SpreiteKitDSL"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SpriteKitDSL",
+            dependencies: [],
+            path: "."),
+//        .testTarget(
+//            name: "SpriteKitDSLTests",
+//            dependencies: ["SpriteKitDSL"],
+//            path: "Tests"
+//        )
+    ]
+)
